9061ce0058e27c46d3411ddbc3cab1612de73a70
gabearmpa <55257018+gabearmpa@users.noreply.github.com>
2020-03-23 13:05:28 -0400

    Create napsack.java

---
+++ src/napsack/napsack.java
@@ -0,0 +1,15 @@
+package cli;
+
+import java.util.ArrayList;
+
+public class napsack {
+
+    public ArrayList<Integer> orderList;
+    public ArrayList<Integer> packingList;
+    public boolean skippedOrder;
+    
+
+    public napsack(){
+
+    }
+}
