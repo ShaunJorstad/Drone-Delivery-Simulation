829718bdaaf5117b926b34cd3313d298258c31e7
Curtisww17 <wcvikingdude@gmail.com>
2020-03-27 14:39:13 -0400

    Updated Fifo to support elapsed time

--- src/simulation/Fifo.java
+++ src/simulation/Fifo.java
@@ -10,10 +10,12 @@ public class Fifo {
 	
 	private Queue<PlacedOrder> orders;
 	private float maxWeight;
+	private double timePassed;
 	
 	public Fifo(ArrayList<PlacedOrder> orders) {
 		this.orders = new LinkedList<PlacedOrder>(orders);
 		maxWeight = 12 * 16; //12 pounds, with 16 oz per pound
+		timePassed = 0;
 	}
 	
 	/**
@@ -21,7 +23,7 @@ public class Fifo {
 	 * @return an arrayList of PlacedOrders that represents the next
 	 * shipment of orders.
 	 */
-	public ArrayList<PlacedOrder> packDrone() {
+	public ArrayList<PlacedOrder> packDrone(double elapsedTime) {
 		
 		ArrayList<PlacedOrder> drone = new ArrayList<PlacedOrder>();
 		Boolean isFull = false;
@@ -30,16 +32,29 @@ public class Fifo {
 		while (!orders.isEmpty() && !isFull) {
 			
 			if (currentWeight + orders.peek().getWeight() <= maxWeight) {
-				
-				currentWeight += orders.peek().getWeight();
-				drone.add(orders.remove());
-				isFull = false;
+				if (orders.peek().getOrderedTime() < (int)elapsedTime) {
+					currentWeight += orders.peek().getWeight();
+					drone.add(orders.remove());
+					isFull = false;
+				} else if (drone.size() == 0){
+
+					timePassed = elapsedTime - (double)orders.peek().getOrderedTime();
+					currentWeight += orders.peek().getWeight();
+					drone.add(orders.remove());
+					isFull = true;
+				}
 				
 			} else {
 				isFull = true;
 			}//else
 		}//while
-		
+		if (orders.size() == 0) return null;
 		return drone;
+	}
+	
+	public double getTimeSkipped() {
+		double out = timePassed;
+		timePassed = 0;
+		return out;
 	}
 }
