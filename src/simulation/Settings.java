ba2c06d3146859cfd9bb4f9fc5b339cf1b1df6f9
Curtisww17 <wcvikingdude@gmail.com>
2020-04-27 09:22:40 -0400

    Added adding, removing, editing map points

--- src/simulation/Settings.java
+++ src/simulation/Settings.java
@@ -242,38 +242,47 @@ public class Settings {
     // Map ----------------------------------------------------------------------
 
     /**
-     * TODO: write this method
      * adds map point
      *
+     * @param name
      * @param x
      * @param y
-     * @param dist
      */
-    private void addMapPoint(int x, int y, float dist) {
-
+    private void addMapPoint(String name, int x, int y) throws IllegalArgumentException {
+    	for (int i = 0; i < map.size(); i++) {
+    		if (map.get(i).getDestName().equals(name))
+    			throw new IllegalArgumentException("That name already exists on the map")
+    	}
+    	Destination newDest = new Destination(name,x,y);
+    	map.add(newDest);
     }
 
     /**
-     * TODO: write ths method
      * removes map point
      *
      * @param x
      * @param y
      */
-    private void removeMapPoint(int x, int y) {
-
+    private void removeMapPoint(int x, int y) throws IllegalArgumentException {
+    	for (int i = 0; i < map.size(); i++) {
+    		if (map.get(i).getDestName().equals(name)) {
+    			map.remove(i);
+    			return;
+    		}
+    	}
+    	throw new IllegalArgumentException("That name does not exist on the map")
     }
 
     /**
-     * TODO: write this method
      * edits map point
      *
+     *@param dest
      * @param x
      * @param y
-     * @param dist
      */
-    private void editMapPoint(int x, int y, float dist) {
-
+    private void editMapPoint(Destination dest, int x, int y) {
+    	dest.setX(x);
+    	dest.setY(y);
     }
 
     /**
