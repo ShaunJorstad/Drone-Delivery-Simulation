db339d1263b889a8654be424fd2b1aa5b12cfdf8
Curtisww17 <wcvikingdude@gmail.com>
2020-05-02 17:17:22 -0400

    Allows for importing/exporting maps, as well as making new ones

--- src/simulation/Settings.java
+++ src/simulation/Settings.java
@@ -31,14 +31,14 @@ public class Settings {
     private static double scale;
     private static Coordinate homeGUILoc;
     private static String mapFileName;
-    private static String mapImage;
 
     private Settings() {
         map = new ArrayList<>();
         foods = new HashSet<>();
         meals = new ArrayList<>();
         orderDistribution = new ArrayList<>();
-        mapImage = "default.png";
+
+        parseMap(new File (defaultMapPath));
 
         // imports the default settings
         parseSettings(new File(defaultSettingsPath));
@@ -436,11 +436,11 @@ public class Settings {
     }
     
     public static String getMapImage() {
-    	return mapImage;
+    	return mapFileName;
     }
     
     public static void setMapImage(String image) {
-    	mapImage = image;
+    	mapFileName = image;
     }
 
     /**
@@ -475,7 +475,7 @@ public class Settings {
         String info = "";
 
         info += "<m>\t"+mapFileName+"\n";
-        info+= "<s>\t"+ scale+"\t"+ homeGUILoc.getX()+"\t"+homeGUILoc.getY()+"\n";
+        info+= "<s>\t"+ scale+"\t"+ (int)homeGUILoc.getX()+"\t"+ (int)homeGUILoc.getY()+"\n";
         for(Destination d: map){
             info+="<d>\t"+d.getDestName()+"\t"+d.getX()+"\t"+
                     d.getY()+"\t"+ d.getDist()+"\n";
@@ -485,6 +485,7 @@ public class Settings {
 
     public static boolean parseMap(File mapFile){
 
+        System.out.println("Hi");
         map.clear();
         //System.out.print
         try {
@@ -496,12 +497,15 @@ public class Settings {
                 String tag;
                 if (line.hasNext()) {
                     tag = line.next();
+                    System.out.println(tag);
                 } else {
                     break;
                 }
 
                 if(tag.equals("<m>")){
+
                     mapFileName = line.next();
+                    System.out.println(mapFileName);
                 } else if (tag.equals("<d>")) {
                     String name;
                     int x, y;
@@ -525,6 +529,7 @@ public class Settings {
                 line.close();
             }
             s.close();
+            System.out.println(mapFileName);
             return true;
 
         } catch (Exception e) {
@@ -676,7 +681,6 @@ public class Settings {
 //        }
 
 
-        parseMap(new File (defaultMapPath));
 
         orderDistribution.clear();
         foods.clear();
@@ -817,7 +821,7 @@ public class Settings {
     }
 
     public static boolean importMapSettings(Stage stage) {
-        fileChooser.setTitle("Import MapSettings");
+        fileChooser.setTitle("Import Map Settings");
         File file = fileChooser.showOpenDialog(stage);
         if (file != null) {
             // TODO: call method to parse the file and import into settings
